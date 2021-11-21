7094cc23f14546e67210f40114104b841889b333
Oatesha <73292759+Oatesha@users.noreply.github.com>
2021-11-21 11:50:29 +0800

    Basic starting implementation

---
+++ Crytpo bot.py
@@ -0,0 +1,10 @@
+# bot to underake algorithmic trading by Harrison Oates
+# Wont be executing trades as is more of a programming exercise rather than actual bot
+# Pretty much static incorrect data
+
+# Initialise the api using pycoingecko module found from documentation at
+# https://www.coingecko.com/en/api/documentation
+from pycoingecko import CoinGeckoAPI
+
+cg = CoinGeckoAPI()
+
